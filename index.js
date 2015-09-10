0789e04da278a7a12c975f55ba5c25d0db558a45
Vinicius Teixeira <vinicius0026@gmail.com>
2015-09-10 18:54:42 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+'use strict'
+
+var express = require('express'),
+    app = express(),
+    server = require('http').createServer(app),
+    bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.post('/deploy', function (req, res) {
+    console.log(req.body);
+    res.send();
+});
+
+server.listen(5000, function() {
+    console.log('Listening for Deploy Events');
+});
