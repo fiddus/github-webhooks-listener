fb13e97ae7379f5f08a5d47a719a2b5141c871d7
Vinicius Teixeira <vinicius0026@gmail.com>
2015-09-10 19:23:58 -0300

    stopping running app, updating and restarting

--- index.js
+++ index.js
@@ -3,14 +3,39 @@
 var express = require('express'),
     app = express(),
     server = require('http').createServer(app),
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    async = require('async'),
+    shell = require('shelljs'),
+    appPath = '/home/git/case4you-server',
+    appStartCmd = 'forever start app.js',
+    appStopCmd = 'forever stopall';
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 app.post('/deploy', function (req, res) {
-    console.log('req.body', req.body);
-    res.send();
+
+    // If master was updated, do stuff
+    if (req.body.ref && req.body.ref === 'refs/heads/master') {
+        shell.cd(appPath);
+
+        async.series([
+            function stopCurrentRunning (next) {
+                shell.exec(appStartCmd, next);
+            },
+            function npmInstall (next) {
+                shell.exec('npm install', next);
+            },
+            function gitPull (next) {
+                shell.exec('git pull origin master', next);
+            },
+            function runNewVersion () {
+                shell.exec(appStartCmd);
+            }
+        ]);
+    }
+
+    res.send(200);
 });
 
 server.listen(5000, function() {
