9f3b056413249f7d74718fa2c453636621ecfd01
Vinicius Teixeira <vinicius0026@gmail.com>
2015-09-11 20:45:02 -0300

    creating grunt file to run tests

---
+++ Gruntfile.js
@@ -0,0 +1,18 @@
+'use strict';
+
+module.exports = function (grunt) {
+    grunt.loadNpmTasks('grunt-mocha-test');
+
+    grunt.initConfig({
+        mochaTest: {
+            test: {
+                options: {
+                    reporter: 'spec'
+                }
+            },
+            src: ['test/*.js']
+        }
+    });
+
+    grunt.registerTask('test', ['mochaTest']);
+}
