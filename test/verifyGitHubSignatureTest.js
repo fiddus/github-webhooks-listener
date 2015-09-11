a2487a8347c8753f5ab311b6d3b832db747cbcfb
Vinicius Teixeira <vinicius0026@gmail.com>
2015-09-11 20:43:56 -0300

    creating tests for a github signature verifier module

---
+++ test/verifyGitHubSignatureTest.js
@@ -0,0 +1,45 @@
+'use strict';
+
+
+
+var verifyGitHubSignature = require('../lib/verifyGitHubSignature'),
+    expect = require('chai').expect;
+
+describe('Testing verifyGitHubSignature Module', function () {
+
+    it('should return true if signature is correct', function (done) {
+        var secret = 'secret',
+            signature = 'sha1=a887d228b2462e1a9998f3c3a82aa1fb9f45e1f8',
+            req = {
+                header: function () {
+                    return signature;
+                },
+                body: {
+                    empty: 'body'
+                }
+            };
+
+        verifyGitHubSignature.setSecret(secret);
+
+        expect(verifyGitHubSignature.ofRequest(req)).to.equal(true);
+        done();
+    });
+
+    it('should return false if signature is incorrect', function (done) {
+        var secret = 'secret',
+            signature = 'some dumb signature',
+            req = {
+                header: function () {
+                    return signature;
+                },
+                body: {
+                    empty: 'body'
+                }
+            };
+
+        verifyGitHubSignature.setSecret(secret);
+
+        expect(verifyGitHubSignature.ofRequest(req)).to.equal(false);
+        done();
+    });
+});
