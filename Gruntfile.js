b3ace11fa0e43fdd993afe408c59d6fe32015ec5
Vinicius Teixeira <vinicius0026@gmail.com>
2015-09-11 21:46:52 -0300

    added jshint and jscs tasks to gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,17 +2,36 @@
 
 module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-mocha-test');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-jscs');
 
     grunt.initConfig({
         mochaTest: {
+
             test: {
                 options: {
                     reporter: 'spec'
                 }
             },
             src: ['test/*.js']
+        },
+        jshint: {
+            options: {
+                jshintrc: true,
+                ignores: ['node_modules/**/*']
+            },
+            all: ['./**/*.js']
+        },
+
+        jscs: {
+            src: './**/*.js',
+            options: {
+                config: '.jscsrc',
+                excludeFiles: ['node_modules/**/*']
+            }
         }
     });
 
+    grunt.registerTask('lint', ['jshint', 'jscs']);
     grunt.registerTask('test', ['mochaTest']);
-}
+};
