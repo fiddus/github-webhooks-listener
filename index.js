feecc8bd93318b4fed5631dc28864c8fa96bc557
Vinicius Teixeira <vinicius0026@gmail.com>
2015-09-14 10:18:30 -0300

    extracting getConfig to its own module

--- index.js
+++ index.js
@@ -1,52 +1,46 @@
 'use strict';
 
+
 var express = require('express'),
     app = express(),
     server = require('http').createServer(app),
     bodyParser = require('body-parser'),
-    appPath = '/home/git/case4you-server',
-    appStartCmd = 'forever start app.js',
-    appStopCmd = 'forever stop app.js',
-    appUpdateCmd = 'git pull origin master',
-    postUpdateCmd = 'npm install',
     secret = process.env.DEPLOY_LISTENER_SECRET,
     verifyGitHubSignature = require('./lib/verifyGitHubSignature'),
+    getConfig = require('./lib/getConfig'),
     deployTasks = require('./lib/deployTasks');
 
-deployTasks.initConfig({
-    appPath: appPath,
-    stopCmd: appStopCmd,
-    updateCmd: appUpdateCmd,
-    postUpdateCmd: postUpdateCmd,
-    startCmd: appStartCmd
-});
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use(bodyParser.json());
 
 verifyGitHubSignature.setSecret(secret);
 
-app.use(bodyParser.urlencoded({extended: false}));
-app.use(bodyParser.json());
+getConfig(function (config) {
+    deployTasks.initConfig(config);
 
-app.post('/deploy', function (req, res) {
+    app.post(config.route, function (req, res) {
 
-    // Checking if request is authentic
-    if (verifyGitHubSignature.ofRequest(req)) {
+        // Checking if request is authentic
+        if (verifyGitHubSignature.ofRequest(req)) {
 
-        // If master was updated, do stuff
-        if (req.body.ref && req.body.ref === 'refs/heads/master') {
+            // If master was updated, do stuff
+            if (req.body.ref && req.body.ref === 'refs/heads/master') {
 
-            deployTasks.run(function () {
+                deployTasks.run(function () {
+                    res.status(200).send();
+                });
+
+            } else {
+                // if other branches were updated, send 200 only to make github happy...
                 res.status(200).send();
-            });
-
+            }
         } else {
-            // if other branches were updated, send 200 only to make github happy...
-            res.status(200).send();
+            res.status(403).send();
         }
-    } else {
-        res.status(403).send();
-    }
-});
+    });
 
-server.listen(5000, function () {
-    console.log('Listening for Deploy Events');
+    server.listen(config.port, function () {
+        console.log('Listening for Deploy Events');
+    });
 });
